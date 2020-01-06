b471c78324eb5945a6e467309199f30cc9b3cbb9
bencochrane1 <bochrane@gmail.com>
2020-01-06 19:11:16 +1100

    change text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Let's build a React aPPPPP
+          Let's build a React APP!!!!!
         </p>
         <a
           className="App-link"
